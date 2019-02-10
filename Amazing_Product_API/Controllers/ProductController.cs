0399c39af62a444bffee6fcfb218bb3def760249
casali1 <frank_casali@hotmail.com>
2019-02-10 16:54:53 -0700

    Added files and references.

---
+++ Amazing_Product_API/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Amazing_Product_API.Models;
+
+
+namespace Amazing_Product_API.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public static List<Product> products = new List<Product>();
+        public static int PageLoadFlag = 1;
+
+        // GET: api/Product
+        public List<Product> GetAllProducts()
+        {
+            if (PageLoadFlag == 1)
+            {
+                var data = new DataStorage();
+                PageLoadFlag++;
+                products = data.GetData();
+                return products;
+            }
+
+            return products;
+        }
+
+        // GET: api/Product/5
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(x => x.ID == id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        // POST: api/Product
+        public void Post(Product product)
+        {
+            product.ID = products.Count + 1;
+            products.Add(product);
+        }
+
+        // PUT: api/Product/5
+        public void Put(int id, Product product)
+        {
+            var record = products.FirstOrDefault(x => x.ID == id);
+
+            if (record != null)
+            {
+                products.Remove(record);
+                products.Add(product);
+            }
+        }
+
+        // DELETE: api/Product/5
+        public void Delete(int id)
+        {
+            foreach (var product in products)
+            {
+                if (product.ID == id)
+                {
+                    products.Remove(product);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Amazing_Product_API/Models/DataStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Amazing_Product_API.Models
+{
+    public class DataStorage
+    {
+        public List<Product> GetData()
+        {
+            return new List<Product>
+            {
+                new Product{ID=1, Name="Truck", Category="Toy", Price=18 },
+                new Product{ID=2, Name="Checkers", Category="Game", Price=5},
+                new Product{ID=3, Name="Soldier", Category="Toy", Price=2 }
+            };
+        }
+    }
+}
